aa27aebd41749a0760ba1cd91052ce67030083e5
Ali Haider Ahmad <102966515+AliHaiderAhmad001@users.noreply.github.com>
2023-08-29 22:31:18 +0300

    positional embeddings layer

---
+++ positional_embeddings.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+class PositionalEmbeddings(tf.keras.layers.Layer):
+    """
+    PositionalEmbeddings layer.
+
+    This layer generates positional embeddings based on input IDs.
+    It uses an Embedding layer to map position IDs to position embeddings.
+
+    Args:
+        config (object): Configuration object containing parameters.
+    """
+
+    def __init__(self, config, name = None, **kwargs):
+        super(PositionalEmbeddings, self).__init__(name=name, **kwargs)
+        self.positional_embeddings = tf.keras.layers.Embedding(
+            input_dim=config.sequence_length, output_dim=config.hidden_size
+        )
+
+    def call(self, input_ids):
+        """
+        Generate positional embeddings.
+
+        Args:
+            input_ids (tf.Tensor): Input tensor containing token IDs.
+
+        Returns:
+            tf.Tensor: Positional embeddings tensor of shape (batch_size, seq_length, hidden_size).
+        """
+        seq_length = input_ids.shape[1]
+        position_ids = tf.range(seq_length, dtype=tf.int32)[tf.newaxis, :]
+        position_embeddings = self.positional_embeddings(position_ids)
+        return position_embeddings
+
+    def get_config(self):
+        """
+        Get the layer configuration.
+
+        Returns:
+            dict: Dictionary containing the layer configuration.
+        """
+        config = super().get_config()
+        config.update({
+            "positional_embeddings": self.positional_embeddings,
+        })
+        return config
