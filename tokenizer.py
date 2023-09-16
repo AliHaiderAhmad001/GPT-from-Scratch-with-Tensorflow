ca6ad81f3d54ba2722efb8efc6b5140a549fa5ae
Ali Haider Ahmad <102966515+AliHaiderAhmad001@users.noreply.github.com>
2023-09-16 15:57:33 +0300

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -27,14 +27,14 @@ class EnglishDataTokenizer(DataTokenizer):
 
     Args:
         tokenizer_path (str): The name or path of the pre-trained tokenizer.
-        max_length (int): The maximum length of tokenized sequences.
+        sequence_length (int): The maximum length of tokenized sequences.
 
     Example:
         tokenizer_path = "tokenizer/adapted-tokenizer"
-        max_length = 10
+        sequence_length = 10
 
         # Create a Tokenizer instance
-        tokenizer = EnglishDataTokenizer(tokenizer_path, max_length)
+        tokenizer = EnglishDataTokenizer(tokenizer_path, sequence_length)
 
         # Test sentence
         test_sentence = "This is a test sentence for tokenization."
@@ -46,14 +46,14 @@ class EnglishDataTokenizer(DataTokenizer):
         print("Tokenized Input IDs:", tokenized_input_ids)
     """
 
-    def __init__(self, tokenizer_path, max_length):
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, pad_token="[PAD]")
-        self.max_length = max_length
+    def __init__(self, tokenizer_path, sequence_length):
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, pad_token="[PAD]", pad_token_id=0)
+        self.sequence_length = sequence_length
 
     def tokenize(self, sentence):
         tokenized_sentence = self.tokenizer(sentence,
                                             padding='max_length',
-                                            max_length=self.max_length,
-                                            return_tensors="tf")
+                                            max_length=self.sequence_length + 1, # For shifting
+                                            truncation=True,
+                                            return_tensors="np")
         return tokenized_sentence['input_ids'][0]
-
