b2059a63ccc73852f43747e8c125685f3cd1a005
Ali Haider Ahmad <102966515+AliHaiderAhmad001@users.noreply.github.com>
2023-09-22 13:24:03 +0300

    Update metrices.py

--- metrices.py
+++ metrices.py
@@ -1,12 +1,42 @@
+# Assumes that 50357 is the PAD token ID.
+
 import tensorflow as tf
 
 class Perplexity(tf.keras.metrics.Metric):
+    """
+    Custom metric for calculating perplexity.
+
+    Perplexity is a measure of how well a probability distribution or probability model predicts a sample.
+    It is commonly used in natural language processing tasks to evaluate the quality of language models.
+
+    Attributes:
+        name (str): Name of the metric.
+    """
+
     def __init__(self, name='perplexity', **kwargs):
+        """
+        Initializes the Perplexity metric.
+
+        Args:
+            name (str, optional): Name of the metric. Defaults to 'perplexity'.
+            **kwargs: Additional keyword arguments passed to the base class.
+        """
         super(Perplexity, self).__init__(name=name, **kwargs)
         self.loss_sum = self.add_weight(name='loss_sum', initializer='zeros')
         self.count = self.add_weight(name='count', initializer='zeros')
 
     def update_state(self, y_true, y_pred, sample_weight=None):
+        """
+        Updates the metric state based on true and predicted values.
+
+        Args:
+            y_true (tensor): True target values.
+            y_pred (tensor): Predicted values.
+            sample_weight (tensor, optional): Optional weighting of samples. Not used.
+
+        Returns:
+            None
+        """
         loss = tf.keras.losses.sparse_categorical_crossentropy(y_true, y_pred)
         mask = y_true != 50357
         # Apply the mask to ignore padded tokens in the loss calculation
@@ -16,25 +46,10 @@ class Perplexity(tf.keras.metrics.Metric):
         self.count.assign_add(tf.reduce_sum(tf.cast(mask, tf.float32)))
 
     def result(self):
+        """
+        Computes the final perplexity metric.
+
+        Returns:
+            tensor: The computed perplexity value.
+        """
         return tf.pow(2.0, self.loss_sum / self.count)
-
-
-class MaskedAccuracy(tf.keras.metrics.Metric):
-    def __init__(self, name='masked_accuracy', **kwargs):
-        super(MaskedAccuracy, self).__init__(name=name, **kwargs)
-        self.total_matches = self.add_weight(name='total_matches', initializer='zeros')
-        self.total_tokens = self.add_weight(name='total_tokens', initializer='zeros')
-
-    def update_state(self, y_true, y_pred, sample_weight=None):
-        pred_labels = tf.argmax(y_pred, axis=2)
-        y_true = tf.cast(y_true, pred_labels.dtype)
-        match = y_true == pred_labels
-        mask = y_true != 50357
-        match = match & mask
-        match = tf.cast(match, dtype=tf.float32)
-        mask = tf.cast(mask, dtype=tf.float32)
-        self.total_matches.assign_add(tf.reduce_sum(match))
-        self.total_tokens.assign_add(tf.reduce_sum(mask))
-
-    def result(self):
-        return self.total_matches / self.total_tokens
