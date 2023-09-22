0c6813681eb06a7acc963540475041153a807807
Ali Haider Ahmad <102966515+AliHaiderAhmad001@users.noreply.github.com>
2023-09-22 13:23:02 +0300

    Update lr_schedule.py

--- lr_schedule.py
+++ lr_schedule.py
@@ -23,11 +23,11 @@ class LrSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         get_config: Returns the configuration dictionary of the learning rate schedule.
     """
 
-    def __init__(self, config, total_number_of_training_samples):
+    def __init__(self, config):
         super(LrSchedule, self).__init__()
         self.warmup_steps = config.warmup_steps
         self.max_learning_rate = config.max_learning_rate
-        self.total_steps = config.num_epochs * (total_number_of_training_samples // config.batch_size)
+        self.total_steps = config.num_epochs * (config.total_number_of_training_samples // config.batch_size)
         self.learning_rate_schedule = tf.keras.optimizers.schedules.PolynomialDecay(
             initial_learning_rate=0.0,
             decay_steps=self.warmup_steps,
@@ -66,10 +66,13 @@ class LrSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         Returns:
             Configuration dictionary.
         """
+        learning_rate_schedule_config = tf.keras.optimizers.schedules.serialize(self.learning_rate_schedule)
+        cosine_schedule_config = tf.keras.optimizers.schedules.serialize(self.cosine_schedule)
+
         return {
             "warmup_steps": self.warmup_steps,
             "max_learning_rate": self.max_learning_rate,
             "total_steps": self.total_steps,
-            "learning_rate_schedule": self.learning_rate_schedule,
-            "cosine_schedule": self.cosine_schedule,
+            "learning_rate_schedule_config": learning_rate_schedule_config,
+            "cosine_schedule_config": cosine_schedule_config,
         }
